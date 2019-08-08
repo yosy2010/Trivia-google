5a8d1e47fe7427958ffaa2e41177452b2a65cc1b
YASSER ALTAMIMI <yosy2010@gmail.com>
2019-08-08 21:56:55 +0300

    added title fragment and inisiate it properly

---
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,35 @@
+package com.example.android.navigation
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        // create a binding object
+        val binding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false) as FragmentTitleBinding
+
+        // return it
+        return binding.root
+    }
+
+
+}
