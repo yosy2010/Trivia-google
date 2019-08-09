c963e1b40467085cb0bb9b482001f2b8a5bda1a6
YASSER ALTAMIMI <yosy2010@gmail.com>
2019-08-09 15:13:03 +0300

    added the action bar and connected it with the about page for now

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -2,13 +2,12 @@ package com.example.android.navigation
 
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 
@@ -25,9 +24,26 @@ class TitleFragment : Fragment() {
             view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
         }
 
+        // enable the option menu
+        setHasOptionsMenu(true)
+
         // return it
         return binding.root
     }
 
+    // this method is called if the setHasOptionMenu is set to true and ti is used to inflate the whole menu
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        // just inflate the menu lay out using the passed inflater
+        inflater.inflate(R.menu.option_menu, menu)
+    }
+
+    // this method is called when the user taps on an item to decide what to do with hist click
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        // return whatever has the same id in the navController
+        return NavigationUI.onNavDestinationSelected(item, view!!.findNavController())
+                || super.onOptionsItemSelected(item)
+    }
 
 }
