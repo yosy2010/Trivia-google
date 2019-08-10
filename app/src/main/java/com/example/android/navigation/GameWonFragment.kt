ef83399fd8ef037db24b01fdf6f1674d3fd00dd1
YASSER ALTAMIMI <yosy2010@gmail.com>
2019-08-10 06:36:57 +0300

    finished the lesson aftaer implemnting the share button in the GameWonFragment

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -16,10 +16,9 @@
 
 package com.example.android.navigation
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -42,9 +41,61 @@ class GameWonFragment : Fragment() {
         // get the passed args
         val args = GameWonFragmentArgs.fromBundle(arguments!!)
 
-        // let's toast it for now
-        Toast.makeText(context, "num correct: ${args.numCorrect} num questions ${args.numQuestions}", Toast.LENGTH_LONG).show()
+        // for the share button
+        setHasOptionsMenu(true)
 
         return binding.root
+    }
+
+    // create the share intent and return it
+    private fun getShareIntent() : Intent{
+
+        // get the arguments passed
+        val args = GameWonFragmentArgs.fromBundle(arguments!!)
+
+        // create an intent
+        val shareIntent = Intent(Intent.ACTION_SEND)
+
+        // continue building the intent
+
+        // the type of the intent
+        shareIntent.type = "text/plain"
+
+        // set the data to be sent
+        shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+
+        // and return it
+        return shareIntent
+    }
+
+    // start an activity using the intent created in the method above
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+    }
+
+    // to inflate the winner menu
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        // inflate the menu
+        inflater.inflate(R.menu.winner_menu, menu)
+
+        // check if there is an application that can do the intent
+        // if there isn't then hide the menu
+        if (null == getShareIntent().resolveActivity(activity!!.packageManager)) {
+            // hide the menu
+            menu.findItem(R.id.share)?.isVisible = false
+        }
+    }
+
+    // called when the button is clicked
+    // almost like onClickListener
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        // if the item clicked is the share menu item then share the success
+        when (item.itemId) {
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
